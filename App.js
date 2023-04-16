baada8557a4d0b7f1ea8bebba904ce9c9cb65fdd
변민우 <yja05092@gmail.com>
2023-04-16 16:36:10 +0900

    The Rules of Native
    1. View는 컨테이너 즉, div와 같은 것이다.
    2. react native에 있는 모든 text는 text component에 들어가야 한다.
    3. StyleSheet.create를 사용하는 이유는 css의 컴포넌트화와 자동 완성 기능 때문에 사용한다.

--- App.js
+++ App.js
@@ -3,18 +3,25 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
+    // View는 컨테이너 즉, div와 같은 것이다.
+    // react native에 있는 모든 text는 text component에 들어가야 한다.
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text style={styles.text}>Hello World~</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// StyleSheet.create를 사용하는 이유는 css의 컴포넌트화와 자동 완성 기능 때문에 사용한다.
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontSize: 50,
+    color: "tomato",
   },
 });
