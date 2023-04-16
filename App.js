e693241a32eac82af398da9a9c8188aa88300fc0
변민우 <yja05092@gmail.com>
2023-04-16 17:46:42 +0900

    Layout System

--- App.js
+++ App.js
@@ -5,23 +5,24 @@ export default function App() {
   return (
     // View는 컨테이너 즉, div와 같은 것이다.
     // react native에 있는 모든 text는 text component에 들어가야 한다.
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World~</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 3, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
     </View>
   );
 }
 
 // StyleSheet.create를 사용하는 이유는 css의 컴포넌트화와 자동 완성 기능 때문에 사용한다.
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    fontSize: 50,
-    color: "tomato",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   text: {
+//     fontSize: 50,
+//     color: "tomato",
+//   },
+// });
